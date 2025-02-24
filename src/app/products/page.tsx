d81871c39f7edf09b0dc5e4fe569e1c323f634b0
wizzzoutMac <alexey@wizzzout.com>
2025-02-24 22:53:48 +0300

    test deploy

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from 'react'
 export default function ProductsPage() {
 	const [products, setProducts] = useState<Product[]>([])
 	const [loading, setLoading] = useState(true)
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [error, setError] = useState<string | null>(null)
 
 	useEffect(() => {
